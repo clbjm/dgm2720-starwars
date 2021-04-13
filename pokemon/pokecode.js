2fb0c57c6cb114058dae9bf0a37fadd1f4e8c92e
jonathon Mullins <jonathonm93@gmail.com>
2021-04-13 10:31:12 -0600

    changed images to display from github

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -2,15 +2,31 @@ const pokeGrid = document.querySelector('.pokeGrid')
 const loadButton = document.querySelector('.loadPokemon')
 const fetchButton = document.querySelector('#fetchSelectedPokemon')
 
+const dialog = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const modalBackground = document.querySelector('.modal-background')
+const submitButton = document.querySelector('#submitButton')
+closeButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+modalBackground.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
+})
+
 loadButton.addEventListener('click', () => {
     loadPage()
 })
 fetchButton.addEventListener('click', () => {
+    dialog.classList.toggle("is-active")
     getAPIData(`https://pokeapi.co/api/v2/pokemon/25`).then(
         (data) => {
             populatePokeCard(data)
         }
     )
+})
+submitButton.addEventListener('click', () => {
+    const inputValue = document.querySelector('.input')
+    console.log(inputValue)
 })
 async function getAPIData(url) {
     try {
@@ -24,7 +40,7 @@ async function getAPIData(url) {
 }
 
 function loadPage() {
-    getAPIData('https:pokeapi.co/api/v2/pokemon?limit=25').then(
+    getAPIData('https:pokeapi.co/api/v2/pokemon?limit=25&offset=748').then(
         async (data) => {
             for (const singlePokemon of data.results) {
                 await getAPIData(singlePokemon.url).then(
@@ -36,7 +52,6 @@ function loadPage() {
 }
 
 function populatePokeCard(singlePokemon) {
-    // console.log(singlePokemon)
     let pokeScene = document.createElement('div')
     pokeScene.className = 'scene'
     let pokeCard = document.createElement('div')
@@ -56,7 +71,7 @@ pokeFront.className = 'card__face card__face--front'
 let frontLabel = document.createElement('p')
 frontLabel.textContent = pokemon.name
 let frontImage = document.createElement('img')
-frontImage.src = `images/${getImageFileName(pokemon)}.png`
+frontImage.src = getImageFileName(pokemon)
 
 pokeFront.appendChild(frontLabel)
 pokeFront.appendChild(frontImage)
@@ -72,9 +87,9 @@ function populateCardBack(pokemon) {
     return pokeBack
 }
 function getImageFileName(pokemon) {
-    if (pokemon.id < 10) {
-        return `00${pokemon.id}`
-    } else if (pokemon.id > 9 && pokemon.id < 100) {
-        return `0${pokemon.id}`
-    }
+    let pokeId
+    if (pokemon.id < 10) pokeId = `00${pokemon.id}`
+    if (pokemon.id > 9 && pokemon.id < 100) pokeId = `0${pokemon.id}`
+    if (pokemon.id > 99 && pokemon.id < 810) pokeId = pokemon.id
+    return `https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/${pokeId}.png`
 }
